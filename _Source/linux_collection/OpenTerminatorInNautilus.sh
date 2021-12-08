49dc50be93e45ee14ebef56a501ae2220f02c4ab
Tung Van TRAN <vttran@linagora.com>
2021-12-09 00:14:01 +0700

    NEW - Linux setting right click open terminator

---
+++ _Source/linux_collection/OpenTerminatorInNautilus.sh
@@ -0,0 +1,6 @@
+# https://askubuntu.com/questions/1138673/is-filemanager-actions-working-with-19-04/1138682#1138682
+# https://askubuntu.com/questions/76712/setting-nautilus-open-terminal-to-launch-terminator-rather-than-gnome-terminal
+sudo apt-get install filemanager-actions
+sudo apt-get install nautilus-actions
+fma-config-tool
+nautilus -q
