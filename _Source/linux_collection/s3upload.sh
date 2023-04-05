cc02885f2ab3b4640883b064da503771524fef29
Tung Van TRAN <vttran@linagora.com>
2023-04-05 15:31:21 +0700

    Update s3upload.sh script - more shortcut - less type

--- _Source/linux_collection/s3upload.sh
+++ _Source/linux_collection/s3upload.sh
@@ -1,4 +1,12 @@
 
 # https://docs.aws.amazon.com/cli/latest/reference/s3api/put-object.html
-aws s3api put-object --bucket tungexplorer --acl public-read --key $2 --body $1
-echo "https://tungexplorer.s3.ap-southeast-1.amazonaws.com/$2"
+if [ -z "$2" ]
+SUFFIX=""
+then
+    SUFFIX="tmp/$1"
+else
+    SUFFIX=$2
+    
+fi
+aws s3api put-object --bucket tungexplorer --acl public-read --key $SUFFIX --body $1
+echo "https://tungexplorer.s3.ap-southeast-1.amazonaws.com/$SUFFIX"
