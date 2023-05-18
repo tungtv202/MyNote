7ad7722bb80912c0abe0af23eec2828702e2cca4
Tung Tran <vttran@linagora.com>
2023-05-18 22:28:47 +0700

    fixup! [NEW] Git alias

--- _Source/linux_collection/git_alias.sh
+++ _Source/linux_collection/git_alias.sh
@@ -5,7 +5,6 @@ git config --global alias.st status
 git config --global alias.rss 'reset --soft HEAD^1'
 git config --global alias.rsh 'reset --hard'
 git config --global alias.stp 'stash pop'
-git config --global alias.st 'stash'
 git config --global alias.cl 'clean -fd'
 git config --global alias.f 'fetch'
 git config --global alias.amne 'commit --amend --no-edit --date'
