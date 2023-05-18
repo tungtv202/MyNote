ddd3f2caa416249ff10f4d3be4aea34426f8e0c9
Tung Tran <vttran@linagora.com>
2023-05-18 22:32:32 +0700

    [NEW] Customize mybash for KDE

---
+++ _Source/linux_collection/kde_mybash.sh
@@ -0,0 +1,40 @@
+# PRIVATE_ENVIRONMENT_PATH template -> private_environment_template.sh 
+
+source $PRIVATE_ENVIRONMENT_PATH
+
+java11() {
+    JAVA_HOME=$JAVA_HOME_11
+}
+
+java17() {
+    JAVA_HOME=$JAVA_HOME_17
+}
+
+# Set Java17 by default
+java17
+
+PATH=$PATH:$JAVA_HOME/bin
+PATH=$PATH:$MAVEN_HOME/bin
+
+alias dcd="docker-compose down"
+alias dcu="docker-compose up"
+
+# Maven 
+alias mvnfire="mvn clean install -Dmaven.javadoc.skip=true -DskipTests"
+
+# Git
+alias gitupdate="git pull origin HEAD && git submodule update"
+
+# PC
+alias sdn="sudo shutdown -h now"
+alias rsn="sudo reboot -h now"
+
+# AWS
+alias s3upload="bash $S3UPLOAD_SHELL_PATH"
+
+# mkdir && cd
+mkcdir ()
+{
+    mkdir -p -- "$1" &&
+      cd -P -- "$1"
+}
---
+++ _Source/linux_collection/private_environment_template.sh
@@ -0,0 +1,14 @@
+# Java
+export MAVEN_HOME=TODO
+export JAVA_HOME_11=TODO
+export JAVA_HOME_17=TODO
+
+# AWS
+export S3UPLOAD_SHELL_PATH=TODO
+
+# fshare
+export FSHARE_USERNAME="TODO" # example: tungtv202@gmail.com
+export FSHARE_PASSWORD="TODO" # example: password123
+export FSHARE_APP_KEY="TODO" # example: dMnqMMZMUnN5YpvKENaEhdQQ5tynqddt
+export FSHARE_USER_AGENT="TODO"  # example: Dellxps-9500
+export FSHARE_BEAR_TOKEN="TODO"  # example: efdf39c90189abfbff339ae344c28db5f78c2885
