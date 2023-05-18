b343ec94d03023bb84c5895df04efc07bf33f39a
Tung Tran <vttran@linagora.com>
2023-05-18 21:14:41 +0700

    [NEW] Git alias

---
+++ _Source/linux_collection/git_alias.sh
@@ -0,0 +1,11 @@
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.rss 'reset --soft HEAD^1'
+git config --global alias.rsh 'reset --hard'
+git config --global alias.stp 'stash pop'
+git config --global alias.st 'stash'
+git config --global alias.cl 'clean -fd'
+git config --global alias.f 'fetch'
+git config --global alias.amne 'commit --amend --no-edit --date'
