9778e6987f8af22e85cf016411a889f8a515270f
Tung Tran <vttran@linagora.com>
2023-07-17 18:16:37 +0700

    [UPDATE] Update new api of s3upload

--- _Source/linux_collection/s3upload.sh
+++ _Source/linux_collection/s3upload.sh
@@ -8,5 +8,5 @@ else
     SUFFIX=$2
     
 fi
-aws s3api put-object --bucket tungexplorer --acl public-read --key $SUFFIX --body $1
+aws s3api put-object --bucket tungexplorer --acl public-read --key $SUFFIX --body $1 > /tmp/awslog.txt
 echo "https://tungexplorer.s3.ap-southeast-1.amazonaws.com/$SUFFIX"
